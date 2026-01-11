3669fdd41da3d9926ef1bc23f92f79e41db073fa
AeroGlory <aerogloryyt@gmail.com>
2026-01-10 20:27:21 -0500

    Added more plugins and stuff

--- lua/aeroglory/init.lua
+++ lua/aeroglory/init.lua
@@ -13,3 +13,5 @@ vim.keymap.set('n', '<leader>h', '<C-w>h') -- Left
 vim.keymap.set('n', '<leader>j', '<C-w>j') -- Down
 vim.keymap.set('n', '<leader>k', '<C-w>k') -- Up
 vim.keymap.set('n', '<leader>l', '<C-w>l') -- Right
+vim.keymap.set('n',"<C-d>", "<C-d>zz")
+vim.keymap.set('n', "<C-u>", "<C-u>zz")
