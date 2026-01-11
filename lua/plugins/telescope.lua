a1d101d032f10257ae87085b1945462b317b25c8
AeroGlory <aerogloryyt@gmail.com>
2026-01-10 19:09:31 -0500

    Added more Telescope keybinds
    Yeah

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },},
 		config = function() 
 			vim.keymap.set("n", "<leader>f", require('telescope.builtin').find_files)
+			vim.keymap.set("n", "<leader>fo", require('telescope.builtin').lsp_outgoing_calls)
+			vim.keymap.set("n", "<leader>fi", require('telescope.builtin').lsp_incoming_calls)
 		end
-    
 }
