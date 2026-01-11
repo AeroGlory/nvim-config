5bdacac3b1e4e525208426ab7d999a6200deaf7a
AeroGlory <aerogloryyt@gmail.com>
2026-01-10 20:07:28 -0500

    Just kidding, here's another color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-	  vim.cmd[[colorscheme tokyonight-storm]]
-  end
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+
+config = function()
+	require("catppuccin").setup({
+    flavour = "mocha", 
+    background = { 
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    float = {
+        transparent = true, -- enable transparent floating windows
+    },
+    auto_integrations = true,
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
+end
 }
