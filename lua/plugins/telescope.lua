09a7eda04c14f17bd0ac69260dfba13fa3556ccb
AeroGlory <aerogloryyt@gmail.com>
2026-01-12 11:59:34 -0500

    Ball

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,8 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },},
 		config = function() 
 			vim.keymap.set("n", "<leader>f", require('telescope.builtin').find_files)
-			vim.keymap.set("n", "<leader>fo", require('telescope.builtin').lsp_outgoing_calls)
-			vim.keymap.set("n", "<leader>fi", require('telescope.builtin').lsp_incoming_calls)
+			vim.keymap.set("n", "<leader>o", require('telescope.builtin').lsp_outgoing_calls)
+			vim.keymap.set("n", "<leader>i", require('telescope.builtin').lsp_incoming_calls)
 		end
+
 }
