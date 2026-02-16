cf5e079124d0ebb97a849456b6ac4e3fd5e93fb1
AeroGlory <aerogloryyt@gmail.com>
2026-02-15 21:35:29 -0500

    Added transparents and removed which-key

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,12 @@ return { "catppuccin/nvim", name = "catppuccin", priority = 1000,
 
 config = function()
 	require("catppuccin").setup({
-    flavour = "mocha", 
+    flavour = "mocha",
+	transparent_background = true,
     background = { 
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = false, -- disables setting the background color.
     float = {
         transparent = true, -- enable transparent floating windows
     },
